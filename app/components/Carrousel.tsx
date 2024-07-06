28f5c741679cc56cb86ed84813d3b45da2689011
Santiago Alvarez Rivera <santiagoalvarive@gmail.com>
2024-07-06 14:23:01 -0500

    Build ready

---
+++ app/components/Carrousel.tsx
@@ -0,0 +1,57 @@
+'use client'
+
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import { Image } from "./interface";
+
+
+interface Props {
+    images: Image[];
+}
+
+var settings = {
+    className: "center",
+    centerMode: true,
+    infinite: true,
+    centerPadding: "80px",
+    slidesToShow: 3,
+    speed: 500,
+    responsive: [
+        {
+            breakpoint: 768,
+            settings: {
+                className: "center",
+                centerMode: true,
+                infinite: true,
+                centerPadding: "60px",
+                slidesToShow: 1,
+                speed: 500,
+            }
+        }
+
+    ]
+};
+
+export const Carrousel = ( {images}: Props ) => {
+
+    return (
+        <>
+            <div>
+                <Slider {...settings}>
+                    {
+                        images.map((image, index) => {
+                            return (
+                                <div key={index} className="carrousel">
+                                    <div className="img">
+                                        <img className="photo" src={image.image} alt="hola" />
+                                    </div>
+                                </div>
+                            )
+                        })
+                    }
+                </Slider>
+            </div>
+        </>
+    )
+}
